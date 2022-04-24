ec9503ea816b5cd5c9c1526cea41e644892fe5ea
Andrew <pasynkov2010@icloud.com>
2022-04-24 18:14:37 +0300

    fix header's link

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return (
         <nav className="orange lighten-0">
             <div className="nav-wrapper">
-                <a href="" className="center brand-logo">
+                <a href="https://guzzlerx.github.io/favorite-movies/" className="center brand-logo">
                     Favorite Movies
                 </a>
             </div>
